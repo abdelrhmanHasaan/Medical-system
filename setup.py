fcb4aa444b49db3e5744dc1ad628580827ac2d15
abdelrhman <bedogabry0987@gmail.com>
2025-12-09 19:27:55 +0200

    zay el ful ya set el kol

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Medical Chatbot",
+    version="0.1.0",
+    author="Abdelrhman Hassan",
+    packages=find_packages(),
+    author_email="abdelrhman14hassan@gmail.com",
+    install_requires = []
+)
